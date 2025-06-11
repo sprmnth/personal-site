e56ec12fcc49727e7f37db993de42a11041a96b3
Saipremnath M <saipremnath@SaipremnathsAir.communityfibre.co.uk>
2025-06-11 23:41:47 +0100

    Fix type error in FoodMap

--- src/components/FoodMap.tsx
+++ src/components/FoodMap.tsx
@@ -6,7 +6,7 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
 // Fix default marker icon issue
-delete (L.Icon.Default.prototype as any)._getIconUrl;
+delete (L.Icon.Default.prototype as unknown as { _getIconUrl: () => string })._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
   iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
